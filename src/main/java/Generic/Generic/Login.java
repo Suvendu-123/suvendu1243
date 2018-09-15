8785a2a0c452a6ca6e32e2c67e8e3cafd20a948b
Suvendu-123 <suvendu.nath12@gmail.com>
2018-09-15 12:55:49 +0530

    fdsf

---
+++ src/main/java/Generic/Generic/Login.java
@@ -0,0 +1,25 @@
+package Generic.Generic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login
+{
+	@Test
+	public void loginTest()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Automation\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.gmail.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.id("identifierId")).sendKeys("suvendu.nath12@gmail.com",Keys.ENTER);	
+		driver.findElement(By.name("password")).sendKeys("Maatarini123",Keys.ENTER);
+	}
+
+}
